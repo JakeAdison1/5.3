5bcb6281eacaa205e9a8967ffd5b5ea20434a6d1
JakeAdison1 <danik.okolot@gmail.com>
2024-05-16 17:26:52 +0300

    new

---
+++ 5.3/5.3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    string input;
+    cout << "Введите строку: ";
+    getline(cin, input);
+
+    string output;
+
+    for (char c : input) {
+        char lower_c = tolower(c);
+        bool is_duplicate = false;
+
+        for (char o : output) {
+            if (lower_c == tolower(o)) {
+                is_duplicate = true;
+                break;
+            }
+        }
+
+        if (!is_duplicate) {
+            output += c;
+        }
+    }
+
+    std::cout << "Строка без повторяющихся символов: " << output << std::endl;
+
+    return 0;
+}
